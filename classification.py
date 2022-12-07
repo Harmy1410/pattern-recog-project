9bfc33038d1e525485ce7b72407ebd52b52b6112
Harmee Patel <31391574+Harmy1410@users.noreply.github.com>
2022-12-07 14:46:27 -0500

    updating

--- classification.py
+++ classification.py
@@ -1,9 +1,9 @@
+from genericpath import exists
 import os
 from collections import deque
-import cProfile
+from keras.saving.legacy.save import load_model
 from tqdm import tqdm
 import numpy as np
-import numpy.typing as npt
 import tensorflow as tf
 
 dev = 1
@@ -17,42 +17,30 @@ else:
 def get_data_from_dir(
     path: str,
 ) -> tuple[tuple[deque, deque], dict[str, int]]:
-    data = {}
+    num_samples = 20_000
+    X = deque()
+    Y = deque()
+    class_map = {}
+    count = 0
     for root, _, files in os.walk(path):
         for file in (pb := tqdm(files)):
             pb.set_description(f"Reading Files")
             if "DS_Store" in file:
                 continue
             key = file[18:-4].lower().replace(" ", "_").replace("-", "_")
-            data[key] = np.load(root + "/" + file, allow_pickle=True)
-            data[key] = tf.reshape(data[key], shape=(data[key].shape[0], 28, 28, 1))
-
-    class_map = {v: idx for idx, v in enumerate(data.keys())}
-
-    # layer = tf.keras.layers.CategoryEncoding(
-    #     num_tokens=len(class_map.keys()), output_mode="one_hot"
-    # )
-    #
-    # one_hot_map = {}
-    # for (idx, v) in enumerate(layer(class_map.keys())):
-    #     one_hot_map[class_map[idx]] = v
-
-    X = deque()
-    Y = deque()
-    for key, items in data.items():
-        arr = items[:5000]
-        X.extend(arr)
-        Y.extend([class_map[key]] * len(arr))
+            class_map[count] = key
+            x = np.load(root + "/" + file, allow_pickle=True)[:num_samples]
+            X.extend(tf.reshape(x, shape=(x.shape[0], 28, 28, 1)))
+            Y.extend([k for k, v in class_map.items() if v == key] * len(x))
+            count += 1
 
     return ((X, Y), class_map)
 
 
 if __name__ == "__main__":
 
-    # pr = cProfile.Profile()
-    # pr.enable()
     # getting data
-    ((X, Y), one_hot_map) = get_data_from_dir(DATASET_PATH)
+    ((X, Y), class_map) = get_data_from_dir(DATASET_PATH)
 
     # converting from np.array is better/performant than from list
     X, Y = tf.convert_to_tensor(np.array(X), dtype=tf.float32), tf.convert_to_tensor(
@@ -95,10 +83,10 @@ if __name__ == "__main__":
             ],
         )
 
-        test_X, test_Y = (
-            X[int(int(tf.shape(X)[0]) * 0.8) :],
-            Y[int(int(tf.shape(Y)[0]) * 0.8) :],
-        )
+    test_X, test_Y = (
+        X[int(int(tf.shape(X)[0]) * 0.8) :],
+        Y[int(int(tf.shape(Y)[0]) * 0.8) :],
+    )
 
     # c1_filters = [64, 32, 64, 32, 64, 32, 64, 32]
     # c1_kernel_size = [3, 5, 5, 3, 5, 5, 3, 3]
@@ -106,8 +94,8 @@ if __name__ == "__main__":
     c1_kernel_size = 3
     c1_strides = 1
     c1_activation = tf.nn.relu
-    m1_strides = [1, 2, 1, 2, 2, 1, 2, 1, 1, 2, 2, 1]
-    m1_pool_size = [2, 1, 2, 1, 1, 2, 1, 2, 1, 2, 2, 1]
+    m1_strides = 1
+    m1_pool_size = 2
 
     # c2_filters = [32, 64, 32, 64, 64, 32, 64, 32]
     # c2_kernel_size = [5, 3, 3, 5, 5, 5, 3, 3]
@@ -115,61 +103,55 @@ if __name__ == "__main__":
     c2_kernel_size = 3
     c2_strides = 1
     c2_activation = tf.nn.relu
-    m2_strides = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
-    m2_pool_size = [2, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 2]
+    m2_strides = 1
+    m2_pool_size = 2
 
     d1_units = 1024
     do_rate = 5e-1
-    d2_units = len(one_hot_map.keys())
+    d2_units = len(class_map.keys())
 
     kernel_initializer = "he_normal"
 
-    lr = 0.002
+    lr = 0.001
     epochs = 16
 
     # model
-    for i in range(len(m1_pool_size)):
-        print(f" ---------- {i} ---------- ")
-        model = tf.keras.models.Sequential()
-        model.add(
-            tf.keras.layers.Conv2D(
-                filters=c1_filters,
-                kernel_size=c1_kernel_size,
-                strides=c1_strides,
-                activation=c1_activation,
-                kernel_initializer=kernel_initializer,
-            )
+    model = tf.keras.models.Sequential()
+    model.add(
+        tf.keras.layers.Conv2D(
+            filters=c1_filters,
+            kernel_size=c1_kernel_size,
+            strides=c1_strides,
+            activation=c1_activation,
+            kernel_initializer=kernel_initializer,
         )
-        model.add(
-            tf.keras.layers.MaxPooling2D(
-                strides=m1_strides[i], pool_size=m1_pool_size[i]
-            )
+    )
+    model.add(tf.keras.layers.MaxPooling2D(strides=m1_strides, pool_size=m1_pool_size))
+    model.add(
+        tf.keras.layers.Conv2D(
+            filters=c2_filters,
+            kernel_size=c2_kernel_size,
+            strides=c2_strides,
+            activation=c2_activation,
+            kernel_initializer=kernel_initializer,
         )
-        model.add(
-            tf.keras.layers.Conv2D(
-                filters=c2_filters,
-                kernel_size=c2_kernel_size,
-                strides=c2_strides,
-                activation=c2_activation,
-                kernel_initializer=kernel_initializer,
-            )
+    )
+    model.add(tf.keras.layers.MaxPooling2D(strides=m2_strides, pool_size=m2_pool_size))
+    model.add(tf.keras.layers.Flatten())
+    model.add(
+        tf.keras.layers.Dense(
+            units=d1_units,
+            activation=tf.nn.relu,
+            kernel_initializer=kernel_initializer,
         )
-        model.add(
-            tf.keras.layers.MaxPooling2D(
-                strides=m2_strides[i], pool_size=m2_pool_size[i]
-            )
-        )
-        model.add(tf.keras.layers.Flatten())
-        model.add(
-            tf.keras.layers.Dense(
-                units=d1_units,
-                activation=tf.nn.relu,
-                kernel_initializer=kernel_initializer,
-            )
-        )
-        model.add(tf.keras.layers.Dropout(rate=do_rate))
-        model.add(tf.keras.layers.Dense(units=d2_units))
+    )
+    model.add(tf.keras.layers.Dropout(rate=do_rate))
+    model.add(tf.keras.layers.Dense(units=d2_units))
 
+    if exists("./checkpoints/model"):
+        model = load_model("./checkpoints/model")
+
+    else:
         model.compile(
             optimizer=tf.optimizers.Adam(learning_rate=lr),
             loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -182,16 +164,22 @@ if __name__ == "__main__":
             epochs=epochs,
             validation_data=(val_X, val_Y),
         )
-        model.summary()
+        model.save_weights("./checkpoints/weights")
+        model.save("./checkpoints/model")
 
-        test_loss, test_acc = model.evaluate(test_X, test_Y)
+    model.summary()
+    test_loss, test_acc = model.evaluate(test_X, test_Y)
 
-        print(f"\ntest_loss: {test_loss}, test_acc: {test_acc}")
+    print(f"\ntest_loss: {test_loss}, test_acc: {test_acc}")
 
-        # write params to file
-        if True:
-            with open("./params2.csv", "a") as file:
-                file.write(
-                    f"{c1_filters}, {c1_kernel_size}, {c1_strides}, {c1_activation.__name__}, {m1_strides[i]}, {m1_pool_size[i]}, {c2_filters}, {c2_kernel_size}, {c2_strides}, {c2_activation.__name__}, {m1_strides[i]}, {m2_pool_size[i]}, {d1_units}, {do_rate}, {d2_units}, {kernel_initializer}, {lr}, {epochs}, {test_acc}, {test_loss}\n"
-                )
-    # pr.disable()
+    pred = model.predict(np.array([test_X[0]]))[0]
+    # print(f"\n prediction       : {pred}")
+    print(f"\n actual_class     : {class_map[pred.tolist().index(max(pred))]}")
+    print(f"\n prediction_class : {class_map[int(test_Y[0].numpy())]}")
+
+    # write params to file
+    if True:
+        with open("./params2.csv", "a") as file:
+            file.write(
+                f"{c1_filters}, {c1_kernel_size}, {c1_strides}, {c1_activation.__name__}, {m1_strides}, {m1_pool_size}, {c2_filters}, {c2_kernel_size}, {c2_strides}, {c2_activation.__name__}, {m1_strides}, {m2_pool_size}, {d1_units}, {do_rate}, {d2_units}, {kernel_initializer}, {lr}, {epochs}, {test_acc}, {test_loss}\n"
+            )
