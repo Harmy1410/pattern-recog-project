c0a150047cb57ec9f838e8adf77ac58b4c1a1e6d
Harmy1410 <harmy1410@gmail.com>
2022-12-07 00:30:10 -0500

    testing different params 😭

--- classification.py
+++ classification.py
@@ -9,9 +9,9 @@ import tensorflow as tf
 dev = 1
 
 if dev == 0:
-    DATASET_PATH = "./dataset"
+    DATASET_PATH = "./dataset/"
 else:
-    DATASET_PATH = "./ds"
+    DATASET_PATH = "./ds/"
 
 
 def get_data_from_dir(
@@ -37,16 +37,12 @@ def get_data_from_dir(
     # for (idx, v) in enumerate(layer(class_map.keys())):
     #     one_hot_map[class_map[idx]] = v
 
-    print(" ---------- creating X, Y ---------- ")
     X = deque()
     Y = deque()
     for key, items in data.items():
-        c = 0
-        for i in items:
-            if c < 5000:
-                X.append(i)
-                Y.append([class_map[key]])
-            c += 1
+        arr = items[:5000]
+        X.extend(arr)
+        Y.extend([class_map[key]] * len(arr))
 
     return ((X, Y), class_map)
 
@@ -56,17 +52,14 @@ if __name__ == "__main__":
     # pr = cProfile.Profile()
     # pr.enable()
     # getting data
-    print(" ---------- reading data ---------- ")
     ((X, Y), one_hot_map) = get_data_from_dir(DATASET_PATH)
 
     # converting from np.array is better/performant than from list
-    print(" ---------- converting to tensors ---------- ")
     X, Y = tf.convert_to_tensor(np.array(X), dtype=tf.float32), tf.convert_to_tensor(
         np.array(Y), dtype=tf.float32
     )
 
     # shuffling
-    print(" ---------- shuffling data ---------- ")
     indices = tf.range(start=0, limit=tf.shape(X)[0])
     shuffled_indices = tf.random.shuffle(indices)
     X, Y = tf.gather(X, shuffled_indices), tf.gather(Y, shuffled_indices)
@@ -76,9 +69,8 @@ if __name__ == "__main__":
     train_X, train_Y, val_X, val_Y, test_X, test_Y = 0, 0, 0, 0, 0, 0
 
     # splitting data
-    print(" ---------- splitting data ---------- ")
     if dev == 1:
-        k = 10_000
+        k = 6400
         j = int(k * 0.8)
         i = int(j * 0.8)
         train_X, train_Y = (X[:i], Y[:i])
@@ -108,17 +100,23 @@ if __name__ == "__main__":
             Y[int(int(tf.shape(Y)[0]) * 0.8) :],
         )
 
-    c1_filters = 8
+    # c1_filters = [64, 32, 64, 32, 64, 32, 64, 32]
+    # c1_kernel_size = [3, 5, 5, 3, 5, 5, 3, 3]
+    c1_filters = 32
     c1_kernel_size = 3
     c1_strides = 1
     c1_activation = tf.nn.relu
-    m1_pool_size = 2
+    m1_strides = [1, 2, 1, 2, 2, 1, 2, 1, 1, 2, 2, 1]
+    m1_pool_size = [2, 1, 2, 1, 1, 2, 1, 2, 1, 2, 2, 1]
 
-    c2_filters = 16
-    c2_kernel_size = 5
+    # c2_filters = [32, 64, 32, 64, 64, 32, 64, 32]
+    # c2_kernel_size = [5, 3, 3, 5, 5, 5, 3, 3]
+    c2_filters = 32
+    c2_kernel_size = 3
     c2_strides = 1
     c2_activation = tf.nn.relu
-    m2_pool_size = 2
+    m2_strides = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+    m2_pool_size = [2, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 2]
 
     d1_units = 1024
     do_rate = 5e-1
@@ -126,66 +124,74 @@ if __name__ == "__main__":
 
     kernel_initializer = "he_normal"
 
-    lr = 1e-3
-    epochs = 32
+    lr = 0.002
+    epochs = 16
 
     # model
-    print(" ---------- defining model ---------- ")
-    model = tf.keras.models.Sequential()
-    model.add(
-        tf.keras.layers.Conv2D(
-            filters=c1_filters,
-            kernel_size=c1_kernel_size,
-            strides=c1_strides,
-            activation=c1_activation,
-            kernel_initializer=kernel_initializer,
-        )
-    )
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=m1_pool_size))
-    model.add(
-        tf.keras.layers.Conv2D(
-            filters=c2_filters,
-            kernel_size=c2_kernel_size,
-            strides=c2_strides,
-            activation=c2_activation,
-            kernel_initializer=kernel_initializer,
-        )
-    )
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=m2_pool_size))
-    model.add(tf.keras.layers.Flatten())
-    model.add(
-        tf.keras.layers.Dense(
-            units=d1_units, activation=tf.nn.relu, kernel_initializer=kernel_initializer
-        )
-    )
-    model.add(tf.keras.layers.Dropout(rate=do_rate))
-    model.add(tf.keras.layers.Dense(units=d2_units))
-
-    print(" ---------- compiling model ---------- ")
-    model.compile(
-        optimizer=tf.optimizers.Adam(learning_rate=lr),
-        loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=["accuracy"],
-    )
-
-    print(" ---------- fitting model ---------- ")
-    history = model.fit(
-        train_X,
-        train_Y,
-        epochs,
-        validation_data=(val_X, val_Y),
-    )
-    model.summary()
-
-    print(" ---------- testing on test data ---------- ")
-    test_loss, test_acc = model.evaluate(test_X, test_Y)
-
-    print(f"\ntest_loss: {test_loss}, test_acc: {test_acc}")
-
-    # write params to file
-    if True:
-        with open("./params.csv", "a+") as file:
-            file.write(
-                f"{c1_filters}, {c1_kernel_size}, {c1_strides}, {c1_activation}, {m1_pool_size}, {c2_filters}, {c2_kernel_size}, {c2_strides}, {c2_activation}, {m2_pool_size}, {d1_units}, {do_rate}, {d2_units}, {kernel_initializer}, {lr}, {epochs}, {test_acc}, {test_loss}"
+    for i in range(len(m1_pool_size)):
+        print(f" ---------- {i} ---------- ")
+        model = tf.keras.models.Sequential()
+        model.add(
+            tf.keras.layers.Conv2D(
+                filters=c1_filters,
+                kernel_size=c1_kernel_size,
+                strides=c1_strides,
+                activation=c1_activation,
+                kernel_initializer=kernel_initializer,
             )
+        )
+        model.add(
+            tf.keras.layers.MaxPooling2D(
+                strides=m1_strides[i], pool_size=m1_pool_size[i]
+            )
+        )
+        model.add(
+            tf.keras.layers.Conv2D(
+                filters=c2_filters,
+                kernel_size=c2_kernel_size,
+                strides=c2_strides,
+                activation=c2_activation,
+                kernel_initializer=kernel_initializer,
+            )
+        )
+        model.add(
+            tf.keras.layers.MaxPooling2D(
+                strides=m2_strides[i], pool_size=m2_pool_size[i]
+            )
+        )
+        model.add(tf.keras.layers.Flatten())
+        model.add(
+            tf.keras.layers.Dense(
+                units=d1_units,
+                activation=tf.nn.relu,
+                kernel_initializer=kernel_initializer,
+            )
+        )
+        model.add(tf.keras.layers.Dropout(rate=do_rate))
+        model.add(tf.keras.layers.Dense(units=d2_units))
+
+        model.compile(
+            optimizer=tf.optimizers.Adam(learning_rate=lr),
+            loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=["accuracy"],
+        )
+
+        history = model.fit(
+            train_X,
+            train_Y,
+            epochs=epochs,
+            validation_data=(val_X, val_Y),
+        )
+        model.summary()
+
+        test_loss, test_acc = model.evaluate(test_X, test_Y)
+
+        print(f"\ntest_loss: {test_loss}, test_acc: {test_acc}")
+
+        # write params to file
+        if True:
+            with open("./params2.csv", "a") as file:
+                file.write(
+                    f"{c1_filters}, {c1_kernel_size}, {c1_strides}, {c1_activation.__name__}, {m1_strides[i]}, {m1_pool_size[i]}, {c2_filters}, {c2_kernel_size}, {c2_strides}, {c2_activation.__name__}, {m1_strides[i]}, {m2_pool_size[i]}, {d1_units}, {do_rate}, {d2_units}, {kernel_initializer}, {lr}, {epochs}, {test_acc}, {test_loss}\n"
+                )
     # pr.disable()
