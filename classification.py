be6cbfc7d04b5ebe084ebb3689c21e8c5384003e
Harmy1410 <harmy1410@gmail.com>
2022-12-09 12:12:48 -0500

    almost final

--- classification.py
+++ classification.py
@@ -1,185 +1,144 @@
 from genericpath import exists
 import os
+from os import listdir
 from collections import deque
-from keras.saving.legacy.save import load_model
 from tqdm import tqdm
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score
 import tensorflow as tf
+from tensorflow.keras.layers import (
+    Input,
+    Conv2D,
+    MaxPooling2D,
+    Dropout,
+    Flatten,
+    Dense,
+    Conv2DTranspose,
+    BatchNormalization
+)
+from tensorflow.keras.models import Model, load_model, Sequential
+from tensorflow.keras.callbacks import EarlyStopping
 
-dev = 1
 
-if dev == 0:
-    DATASET_PATH = "./dataset/"
-else:
-    DATASET_PATH = "./ds/"
+def load_data(root: str = "", int_label=True) -> tuple:
+    labels = []
+    data = None
+    files = [dir for dir in listdir(root) if ".npy" in dir]
+    i = 0
+    for dir in files:
+        label = dir.split(".")[0]
+        temp_data = np.load(root + dir)
+        hot_encoded_label = [0] * len(files)
+        hot_encoded_label[i] = 1
+        labels += (
+            [hot_encoded_label] * temp_data.shape[0]
+            if int_label
+            else [label] * temp_data.shape[0]
+        )
+        data = temp_data if data is None else np.append(data, temp_data, 0)
+        i += 1
+    # The original data are 28, 28 grayscale bitmaps
+    return data.reshape(-1, 28, 28, 1).astype("float") / np.max(data), labels
 
 
-def get_data_from_dir(
-    path: str,
-) -> tuple[tuple[deque, deque], dict[str, int]]:
-    num_samples = 20_000
-    X = deque()
-    Y = deque()
-    class_map = {}
-    count = 0
-    for root, _, files in os.walk(path):
-        for file in (pb := tqdm(files)):
-            pb.set_description(f"Reading Files")
-            if "DS_Store" in file:
-                continue
-            key = file[18:-4].lower().replace(" ", "_").replace("-", "_")
-            class_map[count] = key
-            x = np.load(root + "/" + file, allow_pickle=True)[:num_samples]
-            X.extend(tf.reshape(x, shape=(x.shape[0], 28, 28, 1)))
-            Y.extend([k for k, v in class_map.items() if v == key] * len(x))
-            count += 1
+def get_autoencoder(train_data, test_data) -> tuple:
+    if exists("./content/autoencoder"):
+        model = load_model("./content/autoencoder")
+        encoder = Model(model.input, model.layers[2].output)
+        decoder_input = Input(shape=(7, 7, 3))
+        decoder_layer1 = model.layers[-2](decoder_input)
+        decoder = Model(decoder_input, model.layers[-1](decoder_layer1))
+        return encoder, decoder
 
-    return ((X, Y), class_map)
+    input = Input(shape=(28, 28, 1))
+    layer = Conv2D(32, (4, 4), activation="relu", strides=(2, 2), padding="same")(input)
+    layer = Conv2D(3, (2, 2), activation="sigmoid", strides=(2, 2), padding="same")(
+        layer
+    )
+    layer = Conv2DTranspose(
+        32, (2, 2), activation="relu", strides=(2, 2), padding="same"
+    )(layer)
+    layer = Conv2DTranspose(
+        1, (4, 4), activation="sigmoid", strides=(2, 2), padding="same"
+    )(layer)
+    model = Model(input, layer)
+    model.compile(optimizer="adam", loss="binary_crossentropy")
+
+    callback = EarlyStopping(monitor="loss", patience=3)
+    model.fit(
+        train_data,
+        train_data,
+        epochs=16,
+        batch_size=128,
+        shuffle=True,
+        validation_data=(test_data, test_data),
+        callbacks=[callback],
+    )
+    model.save("./content/autoencoder")
+
+    encoder = Model(model.input, model.layers[2].output)
+    decoder_input = Input(shape=(7, 7, 3))
+    decoder_layer1 = model.layers[-2](decoder_input)
+    decoder = Model(decoder_input, model.layers[-1](decoder_layer1))
+    return encoder, decoder
 
 
 if __name__ == "__main__":
 
-    # getting data
-    ((X, Y), class_map) = get_data_from_dir(DATASET_PATH)
+    train_data, test_data, train_labels, test_labels = train_test_split(
+        *load_data("./ds/"), test_size=0.2
+    )
+    encoder, decoder = get_autoencoder(train_data, test_data)
 
-    # converting from np.array is better/performant than from list
-    X, Y = tf.convert_to_tensor(np.array(X), dtype=tf.float32), tf.convert_to_tensor(
-        np.array(Y), dtype=tf.float32
+    train_data, valid_data, train_labels, valid_labels = train_test_split(
+        train_data, train_labels, test_size=0.2
     )
 
-    # shuffling
-    indices = tf.range(start=0, limit=tf.shape(X)[0])
-    shuffled_indices = tf.random.shuffle(indices)
-    X, Y = tf.gather(X, shuffled_indices), tf.gather(Y, shuffled_indices)
+    encoded_train_data = encoder.predict(train_data)
+    encoded_test_data = encoder.predict(test_data)
+    encoded_valid_data = encoder.predict(valid_data)
 
-    input_shape = (tf.shape(X)[0], tf.shape(X)[1], tf.shape(X)[2], 1)
-
-    train_X, train_Y, val_X, val_Y, test_X, test_Y = 0, 0, 0, 0, 0, 0
-
-    # splitting data
-    if dev == 1:
-        k = 6400
-        j = int(k * 0.8)
-        i = int(j * 0.8)
-        train_X, train_Y = (X[:i], Y[:i])
-        val_X, val_Y = (X[i:j], Y[i:j])
-        test_X, test_Y = (X[j:k], Y[j:k])
-    else:
-        train_X, train_Y = (
-            X[: int(int(int(tf.shape(X)[0]) * 0.8) * 0.8)],
-            Y[: int(int(int(tf.shape(Y)[0]) * 0.8) * 0.8)],
-        )
-
-        val_X, val_Y = (
-            X[
-                int(int(int(tf.shape(X)[0]) * 0.8) * 0.8) : int(
-                    int(tf.shape(X)[0]) * 0.8
-                )
-            ],
-            Y[
-                int(int(int(tf.shape(Y)[0]) * 0.8) * 0.8) : int(
-                    int(tf.shape(X)[0]) * 0.8
-                )
-            ],
-        )
-
-    test_X, test_Y = (
-        X[int(int(tf.shape(X)[0]) * 0.8) :],
-        Y[int(int(tf.shape(Y)[0]) * 0.8) :],
-    )
-
-    # c1_filters = [64, 32, 64, 32, 64, 32, 64, 32]
-    # c1_kernel_size = [3, 5, 5, 3, 5, 5, 3, 3]
-    c1_filters = 32
-    c1_kernel_size = 3
-    c1_strides = 1
-    c1_activation = tf.nn.relu
-    m1_strides = 1
-    m1_pool_size = 2
-
-    # c2_filters = [32, 64, 32, 64, 64, 32, 64, 32]
-    # c2_kernel_size = [5, 3, 3, 5, 5, 5, 3, 3]
-    c2_filters = 32
-    c2_kernel_size = 3
-    c2_strides = 1
-    c2_activation = tf.nn.relu
-    m2_strides = 1
-    m2_pool_size = 2
-
-    d1_units = 1024
-    do_rate = 5e-1
-    d2_units = len(class_map.keys())
-
-    kernel_initializer = "he_normal"
-
-    lr = 0.001
-    epochs = 16
+    train_labels = np.array(train_labels)
+    test_labels = np.array(test_labels)
+    valid_labels = np.array(valid_labels)
 
     # model
-    model = tf.keras.models.Sequential()
-    model.add(
-        tf.keras.layers.Conv2D(
-            filters=c1_filters,
-            kernel_size=c1_kernel_size,
-            strides=c1_strides,
-            activation=c1_activation,
-            kernel_initializer=kernel_initializer,
-        )
-    )
-    model.add(tf.keras.layers.MaxPooling2D(strides=m1_strides, pool_size=m1_pool_size))
-    model.add(
-        tf.keras.layers.Conv2D(
-            filters=c2_filters,
-            kernel_size=c2_kernel_size,
-            strides=c2_strides,
-            activation=c2_activation,
-            kernel_initializer=kernel_initializer,
-        )
-    )
-    model.add(tf.keras.layers.MaxPooling2D(strides=m2_strides, pool_size=m2_pool_size))
-    model.add(tf.keras.layers.Flatten())
-    model.add(
-        tf.keras.layers.Dense(
-            units=d1_units,
-            activation=tf.nn.relu,
-            kernel_initializer=kernel_initializer,
-        )
-    )
-    model.add(tf.keras.layers.Dropout(rate=do_rate))
-    model.add(tf.keras.layers.Dense(units=d2_units))
-
-    if exists("./checkpoints/model"):
-        model = load_model("./checkpoints/model")
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=2,strides=1,activation=tf.nn.relu,kernel_initializer="he_normal"))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Conv2D(filters=16,kernel_size=2,strides=1,activation=tf.nn.relu,kernel_initializer="he_normal"))
+    model.add(BatchNormalization(momentum=0.99))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Flatten())
+    model.add(Dense(units=550, activation=tf.nn.relu))
+    model.add(Dropout(rate=4e-1))
+    model.add(Dense(units=5))
+    if exists("./checkpoints/encodercnn"):
+        model = load_model("./checkpoints/encodercnn")
 
     else:
-        model.compile(
-            optimizer=tf.optimizers.Adam(learning_rate=lr),
-            loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
-            metrics=["accuracy"],
-        )
+        model.compile(optimizer=tf.optimizers.Adam(learning_rate=1e-3),loss="binary_crossentropy", metrics=["accuracy"])
+        model.build(encoded_train_data.shape)
+        model.summary()
 
-        history = model.fit(
-            train_X,
-            train_Y,
-            epochs=epochs,
-            validation_data=(val_X, val_Y),
-        )
-        model.save_weights("./checkpoints/weights")
-        model.save("./checkpoints/model")
+        history = model.fit(encoded_train_data,train_labels,epochs=16,validation_data=(encoded_valid_data, valid_labels))
+        model.save("./checkpoints/encodercnn")
 
-    model.summary()
-    test_loss, test_acc = model.evaluate(test_X, test_Y)
+    test_loss, test_acc = model.evaluate(encoded_test_data, test_labels)
 
-    print(f"\ntest_loss: {test_loss}, test_acc: {test_acc}")
+    char_labels = ['car', 'bird', 'airplane', 'truck', 'ship']
 
-    pred = model.predict(np.array([test_X[0]]))[0]
-    # print(f"\n prediction       : {pred}")
-    print(f"\n actual_class     : {class_map[pred.tolist().index(max(pred))]}")
-    print(f"\n prediction_class : {class_map[int(test_Y[0].numpy())]}")
+    char_test_labels = []
+    for label in test_labels:
+      char_test_labels.append(char_labels[np.where(label == 1)[0][0]])
 
-    # write params to file
-    if True:
-        with open("./params2.csv", "a") as file:
-            file.write(
-                f"{c1_filters}, {c1_kernel_size}, {c1_strides}, {c1_activation.__name__}, {m1_strides}, {m1_pool_size}, {c2_filters}, {c2_kernel_size}, {c2_strides}, {c2_activation.__name__}, {m1_strides}, {m2_pool_size}, {d1_units}, {do_rate}, {d2_units}, {kernel_initializer}, {lr}, {epochs}, {test_acc}, {test_loss}\n"
-            )
+    model_predictions = model.predict(encoded_test_data)
+    char_model_predictions = []
+
+    for label in model_predictions:
+      char_model_predictions.append(char_labels[np.argmax(label)])
+
+    print(f"accuracy_score: \n{accuracy_score(char_test_labels, char_model_predictions)}")
+    print(f"\nroc_auc_score: \n{roc_auc_score(np.array(test_labels), model_predictions)}")
+    print(f"\nconfusion_matrix: \n{confusion_matrix(char_test_labels, char_model_predictions)}")
